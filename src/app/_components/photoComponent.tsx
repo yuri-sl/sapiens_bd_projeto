6a1ec6faff6f8b90b1778a0e99a176345491842f
yuri <yuri03ysl@gmail.com>
2025-07-01 20:24:33 -0300

    Austes rápidos no componente da foto

--- src/app/_components/photoComponent.tsx
+++ src/app/_components/photoComponent.tsx
@@ -1,9 +1,12 @@
 export function PhotoComponent(){
     return(
         <div className="bg-gray-200 text-center min-h-auto font-bold w-full max-w-4xl border-8 border-cyan-700 rounded-lg p-6">
-            <img alt="Imagem do maluco"/>
-            <button className="font-bold text-white bg-green-600">
-                Adicionar nova foto
+            <img src="/assets/user-128.png" alt="Imagem do maluco"/>
+            <button className="bg-green-500  font-bold hover:bg-green-700 px-4 py-2 rounded-md">
+            Adicionar nova foto
+            </button>
+            <button className="bg-red-500  font-bold hover:bg-red-700 px-4 py-2 rounded-md">
+            Remover foto
             </button>
         </div>
     )
